2f120e7ef152a6f8e655fd188c2125211361c469
Neni Diljuita Ndruru <diljuitandruru@gmail.com>
2026-07-28 16:32:40 +0700

    Menambahkan main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import './style.css'
+
+createApp(App)
+.use(router)
+.mount('#app')
