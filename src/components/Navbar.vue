ae65af6f3ab4def3635a2a20aa2b9d0d4adada84
Neni Diljuita Ndruru <diljuitandruru@gmail.com>
2026-07-28 18:07:30 +0700

    Menambahkan Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+
+<nav class="bg-amber-900 text-white shadow">
+
+<div class="max-w-7xl mx-auto flex justify-between items-center p-4">
+
+<RouterLink
+to="/"
+class="text-2xl font-bold"
+>
+
+☕ Coffee Shop
+
+</RouterLink>
+
+<div class="space-x-6">
+
+<RouterLink to="/">Home</RouterLink>
+
+<router-link to="/produk">Menu</router-link>
+
+<RouterLink to="/cart">Pesan</RouterLink>
+
+<RouterLink to="/admin">Admin</RouterLink>
+
+</div>
+
+</div>
+
+</nav>
+
+</template>
