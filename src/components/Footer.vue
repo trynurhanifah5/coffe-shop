2e714087e5623b263fb228eb7df60a28fe1c527c
trynurhanifah <trynurhanifah5@gmail.com>
2026-07-28 15:27:12 +0700

    Menambahkan Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+
+<footer class="bg-amber-900 text-white mt-20">
+
+<div class="max-w-7xl mx-auto py-10 text-center">
+
+<h2 class="text-xl font-bold">
+
+Coffee Shop
+
+</h2>
+
+<p>
+
+©2026 Coffee Shop
+
+</p>
+
+</div>
+
+</footer>
+
+</template>
